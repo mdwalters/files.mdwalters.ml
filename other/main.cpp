b7bd6740659ae4025142e85d261ce6b41185854c
M.D. Walters <popularvsofficial@gmail.com>
2023-02-23 10:26:03 -0500

    Add some weird C++ code

---
+++ other/main.cpp
@@ -0,0 +1,8 @@
+// Some C++ program I made out of boredom
+// I don't even know if this works...
+#include <stdio.h>
+
+int main(let argc, char** argv) {
+    printf("Hello, world!\n");
+    return 0;
+}
